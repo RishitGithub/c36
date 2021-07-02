bde611b53294cf0b21b7fe85ed84745cdfd12c4e
RishitGithub <73820621+RishitGithub@users.noreply.github.com>
2021-07-02 19:02:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,94 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+var feed, lastFed, fedTime;
+
+//create feed and lastFed variable here
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  feed = createButton("Feed The Dog")
+  feed.position(900,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  fedTime = database.ref('feedTime');
+  fedTime.on('value',function(data){
+    lastFed = data.val();
+  })
+ 
+  //write code to display text lastFed time here
+  fill("black");
+  if(lastFed>=12){
+    text("lastFed : "+ lastFed%12 + "PM",380,30)
+  }
+  else if(lastFed == 0){
+    text("lastFed : 12 AM",380,30);
+  }
+  else{
+    text("lastFed : "+lastFed+ "AM",380,30)
+  }
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  //write code here to update food stock and last fed time
+if(foodObj.getFoodStock()<=0){
+  foodObj.updateFoodStock(foodObj.getFoodStock()* 0)
+}
+else{
+  foodObj.deductFood();
+}
+database.ref('/').update({
+  Food: foodObj.getFoodStock(),
+  feedTime: hour()
+})
+
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
